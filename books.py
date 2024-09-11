48e840fd444c5354511f437bfca5d83c6eed26f7
Walmir11 <walmirneto1990@gmail.com>
2024-09-11 16:15:36 -0300

    alterando para read_books

--- books.py
+++ books.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/api-endpoint')
+BOOKS = [
+    {'title': 'Harry Potter','author': 'J.K. Rowling','category': 'fantasy'},
+    {'title': 'Lord of the Rings','author': 'J.R.R. Tolkien','category': 'fantasy'},
+    {'title': 'The Hobbit','author': 'J.R.R. Tolkien','category': 'fantasy'},
+    {'title': 'A Game of Thrones','author': 'George R.R. Martin','category': 'fantasy'},
+    {'title': 'The Catcher in the Rye','author': 'J.D. Salinger','category': 'fiction'},
+    {'title': 'To Kill a Mockingbird','author': 'Harper Lee','category': 'fiction'},
+    {'title': '1984','author': 'George Orwell','category': 'fiction'},
+    {'title': 'Pride and Prejudice','author': 'Jane Austen','category': 'romance'},
+    {'title': 'Gone with the Wind','author': 'Margaret Mitchell','category': 'romance'},
+    {'title': 'Outlander','author': 'Diana Gabaldon','category': 'romance'},
+]
+
+
+@app.get('/books')
 # async é usado para funções assíncronas, ele é opcional
-async def first_api():
-    return {'messege': 'Hello World'}
+async def read_all_books():
+    return BOOKS
 
 
