dd279c093b11c251ac0b0a855ecdd3562abdcf06
Walmir11 <walmirneto1990@gmail.com>
2024-09-12 09:05:22 -0300

    Criando metodos de ler livros

--- Fast_API/books.py
+++ Fast_API/books.py
@@ -21,4 +21,17 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
+@app.get("/books/mybooks")
+async def read_mybooks():
+    return {"books_title": 'My favorite books'}
 
+@app.get('/books/{dynamic_param}')
+async def read_book_category(dynamic_param:str ):
+    return {"dynamic_param": dynamic_param}
+
+@app.get('/books/{book_title}')
+async def read_book(book_title:str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+    return {"error": "Book not found"}
