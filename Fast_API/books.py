646534f59bce33ce5be31ce16cd8a1ab68e6b9d1
Walmir11 <walmirneto1990@gmail.com>
2024-09-13 08:55:09 -0300

    Criando metodos de ler livros pela categoria e autor

--- Fast_API/books.py
+++ Fast_API/books.py
@@ -16,22 +16,40 @@ BOOKS = [
 ]
 
 
-@app.get('/books')
+@app.get('/books/')
 # async é usado para funções assíncronas, ele é opcional
 async def read_all_books():
     return BOOKS
 
-@app.get("/books/mybooks")
+@app.get("/books/mybooks/")
 async def read_mybooks():
     return {"books_title": 'My favorite books'}
 
-@app.get('/books/{dynamic_param}')
-async def read_book_category(dynamic_param:str ):
-    return {"dynamic_param": dynamic_param}
+# @app.get('/books/{dynamic_param}')
+# async def read_book_category(dynamic_param:str ):
+#     return {"dynamic_param": dynamic_param}
 
-@app.get('/books/{book_title}')
+# Traz o livro pelo título
+@app.get('/books/{book_title}/')
 async def read_book(book_title:str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
-    return {"error": "Book not found"}
+
+# Traz os livros pela categoria
+@app.get('/books/{category}/')
+async def book_category(books_category:str):
+    books = []
+    for book in BOOKS:
+        if book.get('category').casefold() == books_category.casefold():
+            books.append(book)
+    return books
+
+# Traz os livros pela categoria e autor
+@app.get('/books/{category}/{author}/')
+async def read_category_author_books(category:str, author:str):
+    books = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold() and book .get('author').casefold() == author.casefold():
+            books.append(book)
+    return books
