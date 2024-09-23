f90e7f101e4042080411bbbc3dde8446b7a1ecb4
Walmir11 <walmirneto1990@gmail.com>
2024-09-23 09:31:36 -0300

    Adicionando funcionalidades

--- Fast_API/project_2/books_2.py
+++ Fast_API/project_2/books_2.py
@@ -19,11 +19,22 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description='ID não precisa ser criado na requisição', default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=1, lt=6)
+
+    model_config = {
+        'json_schema_extra': {
+            'example': {
+                'title': 'Dom Casmurro',
+                'author': 'Machado de Assis',
+                'description': 'Dom Casmurro é um romance escrito por Machado de Assis em 1899.',
+                'rating': 5
+            }
+        }
+    }
 
 BOOKS = [
     Book(1, 'Dom Casmurro', 'Machado de Assis', 'Dom Casmurro é um romance escrito por Machado de Assis em 1899.', 5),
@@ -38,6 +49,21 @@ BOOKS = [
 async def read_books():
     return BOOKS
 
+@app.get('/books/{book_id}')
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    return {'message': 'Book not found'}
+
+@app.get('/books/{book_rating}/')
+async def read_books_by_rating(book_rating: int):
+    books_rating = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_rating.append(book)
+    return books_rating
+
 @app.post('/create_book')
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
@@ -49,3 +75,9 @@ def find_book_id(book: Book):
     else:
         book.id = 1
     return book
+
+@app.put('/book/update_book')
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
