f4c751f20aa13ed17268b5948faeea22720b73ee
Walmir11 <walmirneto1990@gmail.com>
2024-10-09 08:35:24 -0300

    Adicionando legendas

--- Fast_API/project_3/TodoApp/database.py
+++ Fast_API/project_3/TodoApp/database.py
@@ -3,14 +3,22 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Define a URL de conexão com o banco de dados SQLite
+# Esta linha define a URL de conexão para um banco de dados SQLite chamado "todos.db" localizado no diretório atual.
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 # Cria o engine do SQLAlchemy com a URL do banco de dados
-# O argumento connect_args é específico para SQLite
+# O engine é a interface principal para o banco de dados. Ele é responsável por gerenciar as conexões com o banco de dados.
+# O argumento `connect_args={"check_same_thread": False}` é específico para SQLite e permite que a conexão seja compartilhada entre diferentes threads.
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Cria uma fábrica de sessões configurada para não fazer commit ou flush automaticamente
+# `sessionmaker` é uma fábrica de sessões que cria novas instâncias de sessão.
+# `autocommit=False` significa que as transações não serão automaticamente confirmadas.
+# `autoflush=False` significa que as mudanças não serão automaticamente enviadas para o banco de dados.
+# `bind=engine` associa a fábrica de sessões ao engine criado anteriormente.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Cria uma classe base declarativa para os modelos ORM
+# `declarative_base` é uma fábrica que cria uma classe base para os modelos ORM (Object-Relational Mapping).
+# Todos os modelos ORM irão herdar desta classe base.
 Base = declarative_base()
--- Fast_API/project_3/TodoApp/models.py
+++ Fast_API/project_3/TodoApp/models.py
@@ -2,10 +2,20 @@ from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
 class Todos(Base):
+    # Define o nome da tabela no banco de dados como 'todos'
     __tablename__ = 'todos'
 
+    # Define a coluna 'id' como um inteiro, chave primária e indexada
     id = Column(Integer, primary_key=True, index=True)
+
+    # Define a coluna 'title' como uma string
     title = Column(String)
+
+    # Define a coluna 'description' como uma string
     description = Column(String)
+
+    # Define a coluna 'priority' como um inteiro
     priority = Column(Integer)
+
+    # Define a coluna 'complete' como um booleano, com valor padrão False
     complete = Column(Boolean, default=False)
